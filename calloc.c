fddaab8df7ef658fd73ccaf63dc97e7cc79c6529
Késia Silva <kesiasilvab6@gmail.com>
2024-04-23 16:29:24 -0300

    Create calloc.c

---
+++ calloc.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*a funçõa calloc serve para alocar memória durante a execução
+do programa. Ela faz pedido de memória ao computador e retorna 
+um ponteiro com o endereço do inicio do escopo de memória alocado*/
+//protótipo:
+//void* calloc(unsigned int num, unsigned int size)
+int main(){
+	int *p;
+	
+	p = (int *) calloc(5, sizeof(int));
+	
+	if(p == NULL){
+		printf("Erro: Sem Memória!\n");
+		exit(1);//termina o programa
+	}
+	int i;
+	for(i = 0; i<5; i++){
+		printf("digite p[%d]: ", i);
+		scanf("%d", &p[i]);
+	}
+	/*a dunção free() : sempre que alocamos memória é
+	necessário liberá-la quando ela não for mais necessária*/
+	free(p);
+}
