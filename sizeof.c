cb9b97c73b0b0dad22e64bba9f7194bb318b3ac2
Késia Silva <kesiasilvab6@gmail.com>
2024-04-23 15:51:52 -0300

    Update sizeof.c

--- sizeof.c
+++ sizeof.c
@@ -4,6 +4,7 @@
 int main(){
 	/*sizeof: retorna o númweo de bytes necessários para
 	alocar um único elemento de um determinado tipo de dado*/
+	//forma geral: sizeof(nome_do_tipo) ou sizeof(nome_da_variável)
 	int x = sizeof(int);
 	printf("x = %d ", x);//retornou o valor "4" pois, número inteiro possui 4 bytes
 	
